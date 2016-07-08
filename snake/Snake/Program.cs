7decf855431e5b4082ff404a37481079bdbe5ac6
MikeLawrie <metkiym@gmail.com>
2016-07-08 09:59:55 +0500

    Snake1a

---
+++ snake/Snake/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Muxa
+ * Date: 07.07.2016
+ * Time: 17:37
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Snake
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey();
+		}
+	}
+}
