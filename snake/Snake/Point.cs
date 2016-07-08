d32a18af3d798c3fbba34d725d230e078eec512a
MikeLawrie <metkiym@gmail.com>
2016-07-08 12:54:08 +0500

    end lesson3

---
+++ snake/Snake/Point.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Muxa
+ * Date: 08.07.2016
+ * Time: 11:28
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Snake
+{
+	/// <summary>
+	/// Description of Point.
+	/// </summary>
+	public class Point
+	{
+		
+		public int x;
+		public int y;
+		public char sym;
+	
+				public void Draw()
+				
+		{
+			Console.SetCursorPosition(x,y);
+			Console.Write(sym);	
+			
+		}
+		
+		public Point()
+		{
+		
+		}
+	
+		
+	}
+}
