03afe5826fcea6f508315a508e82bce9fac39ead
MikeLawrie <metkiym@gmail.com>
2016-07-09 15:08:59 +0500

    End Lesson 6

--- snake/Snake/Program.cs
+++ snake/Snake/Program.cs
@@ -7,6 +7,10 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Snake
 {
@@ -22,6 +26,39 @@ namespace Snake
 			Point p2 = new Point(4,5,'#');
 			p2.Draw();
 			
+			Point p3 = new Point(10,10, '!');
+			p3.Draw();
+			
+			Point P4 = new Point(14,15,'&');
+			P4.Draw();
+			
+//			List<int> numList = new List<int>();
+//			numList.Add ( 0 );
+//			numList.Add ( 1 );
+//			numList.Add ( 2 );
+//			
+//			int x = numList [ 0 ];
+//			int y = numList [ 1 ];
+//			int z = numList [ 2 ];
+//			
+//			foreach (int i in numList)
+//			{
+//				Console.WriteLine ( i );
+//			}
+			List<Point> pList = new List<Point>();
+			pList.Add(p1);
+			pList.Add(p2);
+			pList.Add(p3);
+			pList.Add(P4);
+			
+			List<char> chList = new List<char>();
+			chList.Add ('a');
+			chList.Add ('b');
+			
+			foreach ( char c in chList)
+			{
+				Console.WriteLine (c);
+			}
 			
 			Console.ReadLine();	
 		}
