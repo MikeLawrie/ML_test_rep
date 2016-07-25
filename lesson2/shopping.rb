7fe9b6e0a9bb53f753086c043200603c6e6993b2
MikeLawrie <metkiym@gmail.com>
2016-07-25 17:13:37 +0500

    lesson2donedone

--- lesson2/shopping.rb
+++ lesson2/shopping.rb
@@ -1,5 +1,6 @@
 shoping = Hash.new 
 name = "v"
+sum=0
 while name != "стоп".to_s
   puts "Введи название товара, либо стоп"
   name = gets.chomp.to_s
@@ -7,8 +8,10 @@ while name != "стоп".to_s
     puts "Введи цену"
     price = gets.chomp.to_i
     puts "Введи количество"
-    number = gets.chomp.to_i
+    number = gets.chomp.to_f
     shoping[name] = {price=>number}
+    sum=sum+(price.to_f*number.to_f)
   end
 end
 puts shoping
+puts sum
