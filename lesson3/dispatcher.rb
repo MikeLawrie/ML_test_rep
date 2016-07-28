88b24d278463d7dbfefa413feaa73a55a6cf9349
MikeLawrie <metkiym@gmail.com>
2016-07-28 17:44:08 +0500

    lesson3add3

--- lesson3/dispatcher.rb
+++ lesson3/dispatcher.rb
@@ -8,7 +8,7 @@ class Station
 	def receive(tr_number,tr_type)
 		puts "Поезд прибыл"
     @trains={tr_number => tr_type}
-    if tr_type == pas then
+    if tr_type == pass then
       @n_pass += 1
     elsif tr_type == cargo then
       @n_cargo += 1
@@ -21,12 +21,20 @@ class Station
 	def train_list_type
     puts "В станции #{@st_name} находится #{n_pass} пассажирских и #{n_cargo} грузовых поездов" 
 	end
-	def train_arrive(tr_number)
+	def train_out(tr_number)
      @trains.delete(tr_number)
+     puts "Поезд #{tr_number} выехал со станции #{st_name}"
+    if @trains[tr_number] == pass then
+      @n_pass -= 1
+    elsif @trains[tr_number] == cargo then
+      @n_cargo -= 1
+    end  
 	end
 end
 
 class Route
+  attr_reader :station_list
+
   def initialize
     @station_list=[]
   end 
@@ -44,43 +52,44 @@ class Route
 	def del_station(st_del)
     @station_list.delete(st_del)
 	end
-	def show_route
-    puts @station_list
-	end
+	
 end
 
 class Train 
   attr_accessor :tr_route
+  attr_reader :tr_number
+  attr_reader :tr_type
+  attr_reader :tr_speed
+  attr_reader :num_carriges
+
   def initialize(tr_number,tr_type,num_carriges)
     @tr_number = tr_number
     @tr_type = tr_type
     @tr_speed = 0
     @num_carriges = num_carriges
-    @tr_route = [].
+    @tr_route = []
     @cur_st = 0
   end
 
 	def speed_up(tr_speed)
     @tr_speed = tr_speed
 	end
-  def show_speed
-    puts @tr_speed
-  end
+ 
   def speed_down
     @tr_speed=0
   end
 
-  def num_carriges_show
-    puts @num_carriges
-  end 
   def add_carrige
     if @tr_speed == 0 then
       @num_carriges+=1
+    else puts "Поезд должен быть неподвижен"
     end
+
   end
   def remove_carrige
     if @tr_speed == 0 then
-      @num_carriges-=1   
+      @num_carriges-=1  
+    else puts "Поезд должен быть неподвижен"   
     end
   end
 
@@ -89,14 +98,23 @@ class Train
 #  end
 
   def move_st
-    @cur_st +=1
+    if @cur_st<tr_route.length 
+      then @cur_st +=1
+      else puts "Поезд находится на конечной станции"
+     end   
   end
-  def show_st
-    if cur_st>0  then 
+  def prev_stat
+    if @cur_st>0  then 
+      prev=@tr_route[(@cur_st-1)]
       puts "Предыдущая станция #{@tr_route[(cur_st-1)]}"
     end
+  end
+  def cur_stat
     puts "Текущая станция #{tr_route[cur_st]}"
-    if cur_st<@tr_route.length then 
+  cur = tr_route[@cur_st]
+  end
+  def next_stat
+    if @cur_st<@tr_route.length then 
       puts "Следующая станция #{@tr_route[(cur_st+1)]}"
     end
   end  
